3b85564d29ca859f685f47da5c758a3acb2c8580
Distant-One <kmarr41@gmail.com>
2017-11-20 21:00:06 -0500

    Changed project format to main file calling individual packages. For example call hex2base64 for the 1st challenge

---
+++ hex2base64/hex2base64.go
@@ -0,0 +1,37 @@
+package hex2base64
+
+import (
+	"fmt"
+)
+
+var infohex2base64 string = "hex2base64.go v001"
+
+var base64Indextable string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+//**************************************
+// Base64 Encode a binary block of data
+//**************************************
+func Bin2base64(inBin []byte) [64]byte {
+	fmt.Println(infohex2base64)
+	var outB64 [64]byte
+	var rounds, i, o int
+
+	i = 0
+	o = 0
+	rounds = len(inBin) / 3
+	// Need to check input bounds - maybe look at how hex.DecodeString() handles variable lenghts
+	// Need to add check for rounds that are do not contain 3 inBin bytes
+	// Do I need to add PAD characters in to indicate last round did not include 3 inBin bytes so decocde will work properly
+	for i < rounds {
+		outB64[o+0] = base64Indextable[((inBin[i*3] >> 2) & 0x3f)]
+		outB64[o+1] = base64Indextable[((inBin[i*3+0]<<4)&0x30)|((inBin[i*3+1]>>4)&0x0f)]
+		outB64[o+2] = base64Indextable[((inBin[i*3+1]<<2)&0x3c)|((inBin[i*3+2]>>6)&0x03)]
+		outB64[o+3] = base64Indextable[(inBin[i*3+2] & 0x3f)]
+		i++
+		o += 4
+	}
+	//	fmt.Printf("Input Vector         : %x\n", inBin)
+	//	fmt.Printf("---Output Base64 Encoded: %s\n", outB64)
+	//	fmt.Printf("Known Answer         : %s\n", base64KnownString)
+	return outB64
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/Distant-One/Challenge/hex2base64"
+)
+
+var hexString string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+var base64KnownString string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+//var base64Indextable string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcportdefghijklmnopqrstuvwxyz0123456789+/"
+
+func main() {
+
+	// For base64 encoding convert ascii hex representation to binary
+	bs, err := hex.DecodeString(hexString)
+	if err != nil {
+		panic(err)
+	}
+
+	//encode binary block to bas64
+	enc64 := hex2base64.Bin2base64(bs)
+
+	fmt.Printf("Input Vector         : %x Len: %d\n", bs, len(bs))
+	fmt.Printf("Output Base64 Encoded: %s len: %d\n", enc64, len(enc64))
+	fmt.Printf("Known Answer         : %s len: %d\n", base64KnownString, len(base64KnownString))
+
+}
